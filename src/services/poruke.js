51ddc4be626f6ae63cce710bfb63e3680a93a4d0
Goran Zaharija <gzaharija@pmfst.hr>
2020-10-15 14:55:00 +0200

    Brisanje poruka

--- src/services/poruke.js
+++ src/services/poruke.js
@@ -1,6 +1,6 @@
 	
 import axios from 'axios'
-const osnovniUrl = 'http://localhost:3001/poruke'
+const osnovniUrl = 'http://localhost:3001/api/poruke'
  
 const dohvatiSve = () => {   
     const promise = axios.get(osnovniUrl);
@@ -14,8 +14,12 @@ const stvori = noviObjekt => {
 const osvjezi = (id, noviObjekt) => {
     return axios.put(`${osnovniUrl}/${id}`, noviObjekt)
 }
+
+const brisi = id => {
+    return axios.delete(`${osnovniUrl}/${id}`)
+}
  
-export default { dohvatiSve, stvori, osvjezi}
+export default { dohvatiSve, stvori, osvjezi, brisi}
 /* export default {
     dohvatiSve: dohvatiSve,
     stvori: stvori,
